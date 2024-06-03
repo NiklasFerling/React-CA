1e7de0a848fef6206e8ea292c2cd24ff6b0f427a
Niklas Ferling <niklas.ferling@gmail.com>
2024-06-04 00:17:29 +0200

    Nav, header, footer, layout, routes

---
+++ my-app/src/components/Contact/index.jsx
@@ -0,0 +1,8 @@
+function Contact() {
+  return (
+    <div>
+      <h1>Contact Us</h1>
+    </div>
+  );
+}
+export default Contact;
---
+++ my-app/src/components/Footer/index.jsx
@@ -0,0 +1,10 @@
+import { Link } from "react-router-dom";
+
+function Footer() {
+  return (
+    <footer className="bg-neutral-900 text-white p-20">
+      <Link to="/contact">Contact Us</Link>
+    </footer>
+  );
+}
+export default Footer;
