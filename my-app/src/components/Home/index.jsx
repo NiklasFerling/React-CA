7db18ffa490255cbdc833305383f4b1027c0b248
Niklas Ferling <niklas.ferling@gmail.com>
2024-06-06 13:04:32 +0200

    Home page h1

--- my-app/src/components/Home/index.jsx
+++ my-app/src/components/Home/index.jsx
@@ -10,6 +10,7 @@ function Home() {
   if (error) return <h1>Error</h1>;
   return (
     <div className="min-h-screen px-20">
+      <h1 className="text-4xl font-bold text-center mb-10">Products:</h1>
       <div className="flex flex-wrap justify-center">
         {data.map((product) => (
           <ProductCard key={product.id} product={product} />
